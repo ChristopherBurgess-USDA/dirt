6ed164c7e261c6c94e06caed236b322e50ad7c0a
Chris Burgess <cjb0326@gmail.com>
2021-03-11 19:34:13 +0000

    modified to run through all samples

--- analysis/assembly/assembly_mapped.sh
+++ analysis/assembly/assembly_mapped.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 ## To run: SGE_Batch -c "bash assembly_mapped.sh" -r std_test_mapped -P 4 -M burgesch@oregonstate.edu
 
-
-ref="/dfs/ROOTS/Myrold_Lab/Chris/dirt/assembled/DW-10-A_contigs.fa"
-reads="/dfs/ROOTS/Myrold_Lab/Chris/dirt/filtered/DW-10-A_filtered.fastq.gz"
-
-save="/dfs/ROOTS/Myrold_Lab/Chris/dirt/mapped/test/DW-10-A/"
-
-/home/roots/burgesch/bin/bbmap/bbmap.sh in=$reads ref=$ref bhist=${save}bhist.txt qhist=${save}qhist.txt aqhist=${save}aqhist.txt lhist=${save}lhist.txt ihist=${save}ihist.txt ehist=${save}ehist.txt qahist=${save}qahist.txt indelhist=${save}indelhist.txt mhist=${save}mhist.txt gchist=${save}gchist.txt idhist=${save}idhist.txt scafstats=${save}scafstats.txt out=${save}mapped.sam t=4
+ref_path="/dfs/ROOTS/Myrold_Lab/Chris/dirt/assembled/"
+reads_path="/dfs/ROOTS/Myrold_Lab/Chris/dirt/filtered/"
+save_path="/dfs/ROOTS/Myrold_Lab/Chris/dirt/mapped/"
+while read sample_id; do
+  ref="${ref_path}${sample_id}_contigs.fa"
+  reads="${reads_path}${sample_id}_filtered.fastq.gz"
+  /home/roots/burgesch/bin/bbmap/bbmap.sh in=$reads ref=$ref scafstats=${save}${sample_id}_scafstats.txt out=${save}${sample_id}.bam t=4 &> ${save}${sample_id}_output.txt
+done < sample_id.txt
