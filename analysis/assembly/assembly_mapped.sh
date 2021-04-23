2c9b802dd367a6495e296b579190ad6ec6ea38e1
Chris Burgess <cjb0326@gmail.com>
2021-04-23 01:00:26 +0000

    adding missed sample

--- analysis/assembly/assembly_mapped.sh
+++ analysis/assembly/assembly_mapped.sh
@@ -9,3 +9,6 @@ while read sample_id; do
   reads="${reads_path}${sample_id}_filtered.fastq.gz"
   /home/roots/burgesch/bin/bbmap/bbmap.sh in=$reads ref=$ref scafstats=${save}${sample_id}_scafstats.txt out=${save}${sample_id}.bam t=4 &> ${save}${sample_id}_output.txt
 done < sample_id.txt
+
+
+/home/roots/burgesch/bin/bbmap/bbmap.sh in="/dfs/ROOTS/Myrold_Lab/Chris/dirt/filtered/NR-06-A_filtered.fastq.gz" ref="/dfs/ROOTS/Myrold_Lab/Chris/dirt/assembled/NR-06-A_contigs.fa" scafstats="/dfs/ROOTS/Myrold_Lab/Chris/dirt/mapped/NR-06-A_scafstats.txt" out="/dfs/ROOTS/Myrold_Lab/Chris/dirt/mapped/NR-06-A.bam" t=4 &> "/dfs/ROOTS/Myrold_Lab/Chris/dirt/mapped/NR-06-A_output.txt"
