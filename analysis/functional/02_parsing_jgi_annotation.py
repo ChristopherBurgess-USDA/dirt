4b79bd46b1b6f79183a90dcfabf188d9186811cf
Chris Burgess <cjb0326@gmail.com>
2021-05-14 19:40:26 +0000

    changed titles to conform with DRAM

--- analysis/functional/02_parsing_jgi_annotation.py
+++ analysis/functional/02_parsing_jgi_annotation.py
@@ -1,13 +1,13 @@
 #%%
 import numpy as np
 import pandas as pd
-import os, re
+import os, re, glob, json
 
-data_path = "../../Thesis/dirt/enzyme/"
+if os.getcwd() != "/home/roots/burgesch/dirt":
+    os.chdir("/home/roots/burgesch/dirt")
 
-if os.getcwd() != "/home/tunasteak/box/projects/dirt":
-    os.chdir("/home/tunasteak/box/projects/dirt")
 
+re_pattern = re.compile("\d+\.a\.|\.txt")
 # %%
 
 ## Parsing Functions
@@ -24,7 +24,8 @@ def compile_files(folder_location):
         [pd.Dataframe]: Dataframe with all annotation information for a given sample.
     """
     files_to_parse = [f for f in os.listdir(folder_location)]
-    file_ids = {re.sub(re_pattern, "", f): f for f in co_08_files}
+    file_ids = {re.sub(re_pattern, "", f): f for f in files_to_parse}
+    sample_id = re.sub("raw/|/IMG_Dat\S+", "", folder_location)
 
     ## The gff file is used to link the sequence id with the gene. This is because the abundance of each gene is done at the sequence level and there are multiple genes per sequence.
     gff_file = (
@@ -44,23 +45,23 @@ def compile_files(folder_location):
         folder_location + file_ids["cog"],
         header=None,
         index_col=0,
-        usecols=[0, 1, 2],
-        names=["gene_id", "cog_term", "cog_percent_id"],
+        usecols=[0, 1, 2, 8, 9],
+        names=["gene_id", "cog_id", "cog_percent_id", "cog_eVal", "cog_bitScore"],
     )
     ko_file = pd.read_table(
         folder_location + file_ids["ko"],
         header=None,
         index_col=0,
-        usecols=[0, 2, 3],
-        names=["gene_id", "ko_term", "ko_percent_id"],
+        usecols=[0, 2, 3, 8, 9],
+        names=["gene_id", "kegg_id", "kegg_percent_id", "kegg_eVal", "kegg_bitScore"],
     )
 
     ec_file = pd.read_table(
         folder_location + file_ids["ec"],
         header=None,
         index_col=0,
-        usecols=[0, 2, 3],
-        names=["gene_id", "ec_term", "ec_percent_id"],
+        usecols=[0, 2, 3, 8, 9],
+        names=["gene_id", "ec_id", "ec_percent_id", "ec_eVal", "ec_bitScore"],
     )
 
     pfam = pd.read_table(
@@ -68,7 +69,7 @@ def compile_files(folder_location):
         header=None,
         index_col=0,
         usecols=[0, 1, 7, 8],
-        names=["gene_id", "pfam_id", "pfam_evalue", "pfam_bit_score"],
+        names=["gene_id", "pfam_id", "pfam_eVal", "pfam_bitScore"],
     )
 
     ## Gene product is JGI's way or picking the best annotation.
@@ -119,24 +120,45 @@ def compile_files(folder_location):
         .pipe(pd.merge, ec_file, on="gene_id", how="left")
         .pipe(pd.merge, pfam, on="gene_id", how="left")
         .pipe(pd.merge, taxonomy, on="gene_id", how="left")
+        .assign(fasta=sample_id)
     )
-
+    compiled_data.to_csv("annotations/" + sample_id + "_annotations.tsv", sep="\t")
     return compiled_data
 
 
-# %%
+sample_directories = glob.glob("raw/*/IMG_Data/*.tar.gz")
 
-co_08_files = [f for f in os.listdir("data/raw/CO-08-A")]
-# %%
-re_pattern = re.compile("\d+\.\w\.|\.txt")
+sample_directories = [i.replace(".tar.gz", "/") for i in sample_directories]
 
+functional_data = [compile_files(i) for i in sample_directories]
 
-# %%
-
-# %%
-
+functional_df = pd.concat(functional_data)
 
 # %%
+## DRAm annotation format
+
+dram_df = functional_df.filter(
+    [
+        "seq_id",
+        "fasta",
+        "kegg_id",
+        "kegg_eVal",
+        "kegg_bitScore",
+        "ec_id",
+        "ec_eVal",
+        "ec_bitScore",
+        "cog_id",
+        "cog_eVal",
+        "cog_bitScore",
+        "pfam_id",
+        "pfam_eVal",
+        "pfam_bitScore",
+        "avg_fold",
+    ]
+)
 
 
 # %%
+functional_df.to_csv("annotations/compiled_annotations.tsv", sep="\t")
+
+dram_df.to_csv("annotations/DRAM_annotations.tsv", sep="\t")
